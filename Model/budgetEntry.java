c962c610e949c5e940acbee5e46af4de7ce990d0
John <mceddie@colostate.edu>
2024-02-20 08:03:40 -0700

    Created a loose template

---
+++ Model/budgetEntry.java
@@ -0,0 +1,6 @@
+package Model;
+
+public class budgetEntry {
+    // read input from View
+    // write to users specified file
+}
---
+++ Model/budgetReport.java
@@ -0,0 +1,7 @@
+package Model;
+
+public class budgetReport {
+    // read in users file 
+    // calculate report
+    // send back to View at budgetReport.html
+}
