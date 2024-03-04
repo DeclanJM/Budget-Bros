cad980cccf91db77639cea6e3e8680501eba5e84
Declan McLaughlin <82425277+DeclanJM@users.noreply.github.com>
2024-03-03 17:14:33 -0700

    Reformatted Calculations and created a default constructor

--- Model/Calculations.java
+++ Model/Calculations.java
@@ -1,38 +1,56 @@
 package Model;
+
 import java.util.ArrayList;
+
 public class Calculations {
-private double total;
+    private double total;
 
-public Calculations(double total){
-    this.total = total;
-    
-}
-public static void main(String[] args) {
-      
-}
-public ArrayList<Double> ReadInFile(ArrayList<String> categories) {
-    return null;
-    // returns array of totals for each category keeping  track of order of categories
-    /* call calc total for each category
-     * 
-     * 
-     * 
-     * 
-     * 
-     * 
-     */
 
-}
-public double CalcTotal(ArrayList<Double> categoryTotals){
-// calculate overall total for categories combined
-for (double element : categoryTotals){
-    total += element;
-}
-return total;
-}
-public double CalcPercentage(ArrayList<Double> list){
-    return 0;
-}
+    public Calculations() {
+        this.total = 0;
+    }
 
-//
+    public Calculations(double total) {
+        this.total = total;
+    }
+
+
+    public double getTotal() {
+        return this.total;
+    }
+
+
+    public void setTotal(double newTotal) {
+        this.total = newTotal;
+    }
+
+
+    public ArrayList<Double> ReadInFile(ArrayList<String> categories) {
+        return null;
+        // returns array of totals for each category keeping  track of order of categories
+        /* call calc total for each category
+        * 
+        */
+
+    }
+
+
+    public double CalcTotal(ArrayList<Double> categoryTotals){
+        // calculate overall total for categories combined
+        for (double element : categoryTotals){
+            total += element;
+        }
+        return total;
+    }
+
+
+    public double CalcPercentage(ArrayList<Double> list){
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        //Test different Calculations(double total) methods, eventually will move this to the Test class
+
+    }
 }
