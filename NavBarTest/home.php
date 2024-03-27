0f061247677dd052451310b107a57b1499917993
John <mceddie@colostate.edu>
2024-03-27 05:18:04 -0600

    completed lots of CSS, HTML, and PHP and made login/signup pages public

---
+++ NavBarTest/home.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal - www.codingnepalweb.com -->
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Responsive Navbar | CodingNepal</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+  <script src="newPage.js" nonce="8IBTHwOdqNKAWeKl7plt8g=="></script>
+</head>
+
+<body>
+  <nav>
+    <input type="checkbox" id="check">
+    <label for="check" class="checkbtn">
+      <i class="fas fa-bars"></i>
+    </label>
+    <label class="logo">Budget Bros</label>
+    <ul>
+      <li><a class="active" href="home.php">Home</a></li>
+      <li><a href="../ExpensePage/index.html">About</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Contact</a></li>
+      <li><a class="button" href="login.php">Login</a></li>
+    </ul>
+  </nav>
+  </div>
+  <section class="home">
+    <div class="white-box">
+      <h1>Welcome to Budget Bros Inc.</h1>
+      <h2> Get the most bang for your budget</h2> <!-- Corrected typo in "budget" -->
+      <p> Make quick updates to your budget, customize your own categories,<br>and organize your finances</p>
+      <form action="./login.php" method="POST">
+        <input class="cn" type="submit" value="Login"></input>
+      </form>
+    </div>
+  </section>
+
+
+
+
+</body>
+
+</html>
