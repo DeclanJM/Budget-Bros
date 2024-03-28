0a0114cf776761054934de926fd2eb1138e7bbc9
Declan McLaughlin <82425277+DeclanJM@users.noreply.github.com>
2024-03-28 02:36:52 -0600

    Adding users to the currentUserList.csv file working without duplicates

---
+++ NavBarTest/CsvWriter.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class CsvWriter {
+    public static void writeUserToCsv(String filePath, User user) { 
+        try { 
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true));
+            writer.append('\n');
+            writer.append(user.getName());
+            writer.append(',');
+            writer.append(user.getEmail());
+            writer.append(',');
+            writer.append(user.getPassword());
+    
+            writer.close();
+
+        } 
+        catch (IOException e) { 
+            e.printStackTrace(); 
+        } 
+    }
+}
