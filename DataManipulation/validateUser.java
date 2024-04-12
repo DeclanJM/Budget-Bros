3ea0e524ed0d1cdc168638b5976d1a0f3dc79b7d
Declan McLaughlin <82425277+DeclanJM@users.noreply.github.com>
2024-04-11 20:44:35 -0600

    Restructured FS

---
+++ DataManipulation/validateUser.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.io.*;
+
+
+public class validateUser {
+
+    public static ArrayList<User> readCSV(String filename, String delimiter, boolean firstLine) {
+        CsvReader csvReader = new CsvReader(filename, delimiter);
+        try {
+            ArrayList<User> userList = csvReader.readUsersFromCsv(firstLine);
+            return userList;
+
+        } catch (IOException e) {
+            System.err.println(e);
+            return null;
+        }
+    }
+
+    public static boolean validNewUser(User newGuy, ArrayList<User> currentUserList) {
+        boolean seenName = false;
+        boolean seenEmail = false;
+
+        for(int i = 0; i < currentUserList.size(); i++) {
+            if(currentUserList.get(i).getName().equals(newGuy.getName())) seenName = true;
+            if(currentUserList.get(i).getEmail().equals(newGuy.getEmail())) seenEmail = true; 
+        }
+        if(seenName || seenEmail) return false;
+        return true;
+    }
+
+    public static void registerUser() {
+        String currentUserFile = "../Data/currentUserList.csv";
+        String newUserFile = "../Data/newUser.csv";
+        ArrayList<User> newUserList = readCSV(newUserFile, ",", false);
+        ArrayList<User> currentUserList = readCSV(currentUserFile, ",", true);
+
+        User newGuy = newUserList.get(0);
+
+        boolean heIsValid = validNewUser(newGuy, currentUserList);
+
+        if(heIsValid) {
+            // Add him to current users
+            // currentUserList.add(newGuy);
+            CsvWriter.writeUserToCsv(currentUserFile, newGuy);
+            System.out.println(newGuy);
+        }
+        else {
+            // Do something else
+            // Throw an error?
+        }
+    }
+
+    public static void main(String[] args) {
+        registerUser();
+    }
+}
