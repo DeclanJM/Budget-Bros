558aecf781c183068a20d6d3a70633ccf14c86f7
John <mceddie@colostate.edu>
2024-05-01 16:40:24 -0600

    added form validation

--- View/register.php
+++ View/register.php
@@ -11,18 +11,38 @@
             <input required type='text' class='input-field' name="firstName" placeholder='First Name' id="fname">
             <input required type='text' class='input-field' name="lastName" placeholder='Last Name' id="lname">
             <input required type='email' class='input-field' name="email" placeholder='Email'>
-            <input required type='password' class='input-field' name="first" placeholder='Enter Password' id="fpass" minlength="5">
-            <input required type='password' class='input-field' name="second" placeholder='Confirm Password' id="spass" minlength="5">
-            <input required type='checkbox' class='check-box'> <span>I agree to the terms and conditions</span>
+            <div id="passwords">
+                <input required type='password' class='input-field' name="first" placeholder='Enter Password' id="fpass" minlength="5">
+                <input required type='password' class='input-field' name="second" placeholder='Confirm Password' id="spass" minlength="5">
+            </div>
+            <input required type='checkbox' class='check-box' id='terms'> <span>I agree to the terms and conditions</span>
             <input type='submit' class='submit-btn' name='page' value='Sign Up'>
         </form>
     </div>
 </div>
 <script>
-    $fpass = getElementById("fpass");
-    $spass = getElementById("spass");
-    if ($fpass.value != $spass.value) {
-        $fpass.append("<span style='color:red'>PASSWORDS MUST MATCH</span>");
-        $spass.append("<span style='color:red'>PASSWORDS MUST MATCH</span>");
+    function checkIfPasswordsMatch() {
+        if ($fpass.value != $spass.value && $("#alert").length == 0) {
+            $("#passwords").append("<div style='color:red;font-size: 12px' id='alert'>PASSWORDS MUST MATCH</div>");
+        }
+        else if($fpass.value == $spass.value) {
+            $("#alert").remove();
+        }
     }
+    function checkAllFields(event) {
+        if ($fpass.value != $spass.value || $("#alert").length == 0 || fname.value == '' || lname.value == '' || email.value == '' || terms.value == 0) {
+            event.preventDefault();
+        }
+        alert("All values must be entered and passwords must match");
+    }
+    $fpass = document.getElementById("fpass");
+    $spass = document.getElementById("spass");
+    $spass.onfocusout = function () {
+        checkIfPasswordsMatch();
+    }
+    $spass.onkeyup = function () {
+        checkIfPasswordsMatch();
+    }
+    login.addEventListener("submit", checkAllFields);
+
 </script>
